80f7d6f1d27b52713b6636a962c8bc6fab480894
Albert Albala <albert@mediatribe.net>
2021-11-20 23:20:04 -0500

    2021-11-20-23:20:03

--- my-vue-router-v3.js
+++ my-vue-router-v3.js
@@ -1,83 +1,85 @@
-mountMyApp = function(options = {}) {
-  try {
-    if (options.simulateError === true) {
-      throw 'An error has been simulated';
-    }
-
-    const LineChart = { template: '#vue-route-home'}
-    const BarChart = { template: '#vue-route-barchart'}
-
-    const routes = [
-      {
-        path: '/',
-        component: LineChart,
-        meta: {
-          enterClass: 'animate__animated animate__fadeInLeft',
-          leaveClass: 'animate__animated animate__fadeOutRight',
-        }
-      },
-      {
-        path: '/bar-chart',
-        component: BarChart,
-        meta: {
-          enterClass: 'animate__animated animate__fadeInRight',
-          leaveClass: 'animate__animated animate__fadeOutLeft',
-        }
-      },
-    ]
-
-    // 3. Create the router instance and pass the `routes` option
-    // You can pass in additional options here, but let's
-    // keep it simple for now.
-    const router = VueRouter.createRouter({
-      // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-      history: VueRouter.createWebHashHistory(),
-      routes, // short for `routes: routes`
-    });
-
-    const vueApp = Vue.createApp({
-      watch: {
-        $route() {
-          // https://stackoverflow.com/a/64751397/1207752
-          this.$nextTick(this.routeLoaded);
-        }
-      },
-      methods: {
-        // https://stackoverflow.com/a/64751397/1207752
-        routeLoaded() {
-          switch (this.$route.path) {
-            case '/':
-              displayChart('echart-main', 'line');
-              break;
-            case '/bar-chart':
-              displayChart('echart-bar-chart', 'bar');
-              break;
-          }
-        }
-      },
-      data() {
-        return {
-          greeting: "Hi there!",
-          label: "this page",
-          docsUrl: "https://v3.vuejs.org"
-        };
+MyApp = {
+  mount: function(options = {}) {
+    try {
+      if (options.simulateError === true) {
+        throw 'An error has been simulated';
       }
-    });
 
-    vueApp.use(router);
+      const LineChart = { template: '#vue-route-home'}
+      const BarChart = { template: '#vue-route-barchart'}
 
-    vueApp.mount('#app');
+      const routes = [
+        {
+          path: '/',
+          component: LineChart,
+          meta: {
+            enterClass: 'animate__animated animate__fadeInLeft',
+            leaveClass: 'animate__animated animate__fadeOutRight',
+          }
+        },
+        {
+          path: '/bar-chart',
+          component: BarChart,
+          meta: {
+            enterClass: 'animate__animated animate__fadeInRight',
+            leaveClass: 'animate__animated animate__fadeOutLeft',
+          }
+        },
+      ]
 
-    showHideElements('display-after-load', 'block');
-  }
-  catch (error) {
-    showHideElements('display-if-error', 'block');
-  }
-  showHideElements('display-before-load', 'none');
-}
+      // 3. Create the router instance and pass the `routes` option
+      // You can pass in additional options here, but let's
+      // keep it simple for now.
+      const router = VueRouter.createRouter({
+        // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+        history: VueRouter.createWebHashHistory(),
+        routes, // short for `routes: routes`
+      });
 
-showHideElements = function(classname, display) {
-  for (const element of document.getElementsByClassName(classname)) {
-     element.style.display = display;
-  }
+      this.vue = Vue.createApp({
+        watch: {
+          $route() {
+            // https://stackoverflow.com/a/64751397/1207752
+            this.$nextTick(this.routeLoaded);
+          }
+        },
+        methods: {
+          // https://stackoverflow.com/a/64751397/1207752
+          routeLoaded() {
+            switch (this.$route.path) {
+              case '/':
+                displayChart('echart-main', 'line');
+                break;
+              case '/bar-chart':
+                displayChart('echart-bar-chart', 'bar');
+                break;
+            }
+          }
+        },
+        data() {
+          return {
+            greeting: "Hi there!",
+            label: "this page",
+            docsUrl: "https://v3.vuejs.org"
+          };
+        }
+      });
+
+      this.vue.use(router);
+
+      this.vue.mount('#app');
+
+      this.showHideElements('display-after-load', 'block');
+    }
+    catch (error) {
+      console.error(error);
+      this.showHideElements('display-if-error', 'block');
+    }
+    this.showHideElements('display-before-load', 'none');
+  },
+  showHideElements: function(classname, display) {
+    for (const element of document.getElementsByClassName(classname)) {
+       element.style.display = display;
+    }
+  },
 }
--- my-vue.js
+++ my-vue.js
@@ -1,28 +1,29 @@
-mountMyApp = function(options = {}) {
-  try {
-    if (options.simulateError === true) {
-      throw 'An error has been simulated';
-    }
-    Vue.createApp({
-      data() {
-        return {
-          greeting: "Hi there!",
-          label: "this page",
-          docsUrl: "https://v3.vuejs.org"
-        }
+MyApp = {
+  mount: function(options = {}) {
+    try {
+      if (options.simulateError === true) {
+        throw 'An error has been simulated';
       }
-    }).mount('#app');
-    showHideElements('display-after-load', 'block');
-  }
-  catch (error) {
-    console.error(error);
-    showHideElements('display-if-error', 'block');
-  }
-  showHideElements('display-before-load', 'none');
-}
-
-showHideElements = function(classname, display) {
-  for (const element of document.getElementsByClassName(classname)) {
-     element.style.display = display;
+      this.vue = Vue.createApp({
+        data() {
+          return {
+            greeting: "Hi there!",
+            label: "this page",
+            docsUrl: "https://v3.vuejs.org"
+          }
+        }
+      }).mount('#app');
+      this.showHideElements('display-after-load', 'block');
+    }
+    catch (error) {
+      console.error(error);
+      this.showHideElements('display-if-error', 'block');
+    }
+    this.showHideElements('display-before-load', 'none');
+  },
+  showHideElements: function(classname, display) {
+    for (const element of document.getElementsByClassName(classname)) {
+       element.style.display = display;
+    }
   }
 }
