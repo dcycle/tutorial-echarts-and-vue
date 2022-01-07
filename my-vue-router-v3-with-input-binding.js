e9acefbf9a8ef08c8895fa92c158379c17369e10
Albert Albala <albert@mediatribe.net>
2022-01-07 12:37:34 -0500

    Add input binding in routes

---
+++ my-vue-router-v3-with-input-binding.js
@@ -0,0 +1,94 @@
+MyApp = {
+  mount: function(options = {}) {
+    try {
+      if (options.simulateError === true) {
+        throw 'An error has been simulated';
+      }
+
+      const LineChart = {
+        props: [
+          'greeting',
+          'input_binding',
+        ],
+        template: '#vue-route-home'
+      }
+      const BarChart = {
+        props: [
+          'greeting',
+          'input_binding',
+        ],
+        template: '#vue-route-barchart'
+      }
+
+      const routes = [
+        {
+          path: '/',
+          component: LineChart,
+          meta: {
+            enterClass: 'animate__animated animate__fadeInLeft',
+            leaveClass: 'animate__animated animate__fadeOutRight',
+          }
+        },
+        {
+          path: '/bar-chart',
+          component: BarChart,
+          meta: {
+            enterClass: 'animate__animated animate__fadeInRight',
+            leaveClass: 'animate__animated animate__fadeOutLeft',
+          }
+        },
+      ]
+
+      // 3. Create the router instance and pass the `routes` option
+      // You can pass in additional options here, but let's
+      // keep it simple for now.
+      const router = VueRouter.createRouter({
+        // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+        history: VueRouter.createWebHashHistory(),
+        routes, // short for `routes: routes`
+      });
+
+      this.vue = Vue.createApp({
+        watch: {
+          $route() {
+            // https://stackoverflow.com/a/64751397/1207752
+            this.$nextTick(this.routeLoaded);
+          }
+        },
+        methods: {
+          // https://stackoverflow.com/a/64751397/1207752
+          routeLoaded() {
+            switch (this.$route.path) {
+              case '/':
+                displayChart('echart-main', 'line');
+                break;
+              case '/bar-chart':
+                displayChart('echart-bar-chart', 'bar');
+                break;
+            }
+          }
+        },
+        data() {
+          return {
+            greeting: "Hi there!",
+            input_binding: "This uses input binding.",
+            label: "this page",
+            docsUrl: "https://v3.vuejs.org"
+          };
+        }
+      }).use(router).mount('#app');
+
+      this.showHideElements('display-after-load', 'block');
+    }
+    catch (error) {
+      console.error(error);
+      this.showHideElements('display-if-error', 'block');
+    }
+    this.showHideElements('display-before-load', 'none');
+  },
+  showHideElements: function(classname, display) {
+    for (const element of document.getElementsByClassName(classname)) {
+       element.style.display = display;
+    }
+  },
+}
